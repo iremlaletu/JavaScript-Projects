24988cc2dbec95d6de14983e85b19b54c6ceda3d
iremlaletu <iremlaletu@gmail.com>
2023-12-06 03:44:10 +0300

    Added LeadTracker

--- LeadTracker/main.js
+++ LeadTracker/main.js
@@ -1,35 +1,57 @@
 
-let inputBtn = document.querySelector("#input-btn");
-const inputEl = document.querySelector("#input-el");
 let myLeads = []
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn"); 
+const tabBtn = document.querySelector("#tab-btn")
+const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
+// Getting "myLeads" change it to array storing it in the variable 
 
 
-let leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
-// getting "myLeads" change it to array storing it in the variable 
-console.log(leadsFromStorage)
+if (leadsFromStorage) {
+    myLeads = leadsFromStorage
+    render(myLeads)
+}
 
+
+// For getting the current tab from Google
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    });
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i ++){
+        listItems += ` 
+            <li>
+                <a target='_blank' href="${leads[i]}"> 
+                 ${leads[i]}
+                </a>
+             </li> 
+        `
+    }
+    ulEl.innerHTML = listItems
+} 
+
+// For adding
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) ) 
     //First "myLeads" is the key we want to save our items under, second "myLeads" is the what we actually save. grabing the variable to hold myleads then we need to change to the string.
 
-    renderLeads()
-    console.log( localStorage.getItem("myLeads") )
+    render(myLeads)
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i ++){
-        listItems += ` 
-        <li>
-            <a target='_blank' href="${myLeads[i]}"> 
-                ${myLeads[i]}
-            </a>
-        </li> `
-    }
-    ulEl.innerHTML = listItems
-} 
-
-
+//For deleting listen for double click
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = [] // setting empty array to clear (reassign)
+    render(myLeads)
+})
--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# JavaScript Practices
-
-## Contains my own exercises: Login Form and To-Do-List.
+# JavaScript Projects
+* * *
+## Contains my own projects:
+* * *
+1. Login Form with Signup
+2. To Do List
+3. Chrome Extension (Lead Tracker):
+    * I employed query method along with the Chrome Tab API to interact with the browser tab system(Modifying the manifest). 
+    * Additionally, I implemented the capability to store data in the Local Storage, for saving previous inputs in case of any loss. 
+    * For icons, I used fontawesome.com to download the external resource into my package folder and load the script locally. I did this for the following reason: according to the Chrome Extension documentation, Google Chrome has CSP (Content Security Policy), which means chrome extensions don't allow the external script. 
 
 ## Demo
+* * *
 
 ![login](img/practice-list.gif)
 
 ![todolist](img/login.gif)
+
+![extension](img/extension.gif)
